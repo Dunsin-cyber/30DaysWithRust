1aa877ad23a5513dbb4579304c4034db7bf50c89
Abisuwa <ayodejiabisuwa23@gmail.com>
2023-11-29 11:13:51 +0100

    [feat] - Code that gets a guess from the user and prints it

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,14 @@
-// use n
+use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please type in your guess...");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("you guessed: {guess}");
 }
